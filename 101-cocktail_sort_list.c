697f46f1a15734d234cb62663b5c1782ed2bf872
flowsar <brahim.sar151@gmail.com>
2024-04-29 23:02:55 +0100

    cocktail sort implementation

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,44 +1,82 @@
 #include "sort.h"
 
-listint_t *cocktail_sort_list_handler(listint_t **head, listint_t *node1, listint_t *node2);
-void Swap_nodes_cocktail(listint_t **head, listint_t *node1, listint_t *node2);
-
+listint_t *sort_left_r(listint_t **list, listint_t *node1, listint_t *node2);
+void sort_right_l(listint_t **list, listint_t *node2, listint_t *node1);
+void Swap_nodes_cocktail(listint_t **list, listint_t *node1, listint_t *node2);
+/**
+ * cocktail_sort_list - this algorithm works like bubble sort
+ * with additional rigth to left sort
+ *
+ * @list: linked_list head
+ */
 void cocktail_sort_list(listint_t **list)
 {
-    listint_t *start, *prev;
-    
-    start = (*list)->next;
-    prev = start->prev;
-    
-	Swap_nodes_cocktail(list, prev, start);
-	
-//     cocktail_sort_list_handler(list, prev, start);
-//     while (start)
-//     {
-//         
-//         if (prev != NULL &&start->n < prev->n)
-//         prev = start;
-//         start = start->next;
-//         
-//     }
+	listint_t *prev, *current, *first_node, *last_node;
+
+	first_node = (*list);
+	while (first_node)
+	{
+		prev = (*list);
+		current = prev->next;
+		last_node =  sort_left_r(list, prev, current);
+		sort_right_l(list, last_node->prev, last_node);
+		first_node = first_node->next;
+	}
 }
 
-listint_t *cocktail_sort_list_handler(listint_t **head, listint_t *node1, listint_t *node2)
+/**
+ * sort_left_r - sort from left to right
+ *
+ * @list: linked_list head
+ * @node1: node 1
+ * @node2: node 2
+ *
+ * Return: last last node of a list
+ */
+listint_t *sort_left_r(listint_t **list, listint_t *node1, listint_t *node2)
 {
-// 	if (node2 == NULL)
-// 		return;
-	
-// 	Swap_nodes();
-// 	cocktail_sort_list_handler();
-// 	Swap_nodes();
-	
-}
+	listint_t *prev, *current;
 
-void Swap_nodes_cocktail(listint_t **head, listint_t *node1, listint_t *node2)
+	if (node2 == NULL || node1 == NULL)
+		return (node1);
+
+	prev = node1;
+	current = node2;
+	Swap_nodes_cocktail(list, prev, current);
+	return (sort_left_r(list, current, current->next));
+}
+/**
+ * sort_right_l - sort from right to left
+ *
+ * @list: linked_list head
+ * @node1: node 1
+ * @node2: node 2
+ */
+void sort_right_l(listint_t **list, listint_t *node2, listint_t *node1)
+{
+	listint_t *prev, *current;
+
+	if (node2 == NULL || node1 == NULL)
+		return;
+
+	prev = node1;
+	current = node2;
+	Swap_nodes_cocktail(list, current, prev);
+	sort_right_l(list, current->prev, current);
+}
+/**
+ * Swap_nodes_cocktail - sort from left to right
+ *
+ * @list: linked_list head
+ * @node1: node 1
+ * @node2: node 2
+ *
+ * Return: last last node of a list
+ */
+void Swap_nodes_cocktail(listint_t **list, listint_t *node1, listint_t *node2)
 {
 	if (node1 != NULL && node2 != NULL)
 	{
-		printf("swap");
 		if (node2->n < node1->n)
 		{
 			node1->next = node2->next;
@@ -48,53 +86,10 @@ void Swap_nodes_cocktail(listint_t **head, listint_t *node1, listint_t *node2)
 			if (node1->prev != NULL)
 				node1->prev->next = node2;
 			else
-				*head = node2;
+				*list = node2;
 			node2->next = node1;
 			node1->prev = node2;
+			print_list((*list));
 		}
 	}
-}
-listint_t *create_listint(const int *array, size_t size)
-{
-    listint_t *list;
-    listint_t *node;
-    int *tmp;
-
-    list = NULL;
-    while (size--)
-    {
-        node = malloc(sizeof(*node));
-        if (!node)
-            return (NULL);
-        tmp = (int *)&node->n;
-        *tmp = array[size];
-        node->next = list;
-        node->prev = NULL;
-        list = node;
-        if (list->next)
-            list->next->prev = list;
-    }
-    return (list);
-}
-
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
-    listint_t *list;
-    int array[] = {19, 6, 99, 71, 13, 52, 96, 73, 86, 7};
-    size_t n = sizeof(array) / sizeof(array[0]);
-
-    list = create_listint(array, n);
-    if (!list)
-        return (1);
-    print_list(list);
-    printf("\n");
-    cocktail_sort_list(&list);
-    printf("\n");
-    print_list(list);
-    return (0);
 }
