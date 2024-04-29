3b10638405d1e848411ad4acfc904b475b72e88c
flowsar <brahim.sar151@gmail.com>
2024-04-29 19:00:27 +0100

    improve insert sort swap and add cocktail algori

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -93,6 +93,8 @@ listint_t *swap_nodes(listint_t **head, listint_t *node1, listint_t *node2)
 				*head = node2;
 			node2->next = node1;
 			node1->prev = node2;
+            return (node2);
 		}
 	}
+	return (NULL);
 }
