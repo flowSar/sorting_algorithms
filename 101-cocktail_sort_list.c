0d04116b5ebdd1ff19c06bfd810fe6e5b05af1f6
flowsar <brahim.sar151@gmail.com>
2024-04-30 16:53:04 +0100

    up

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -10,7 +10,7 @@ void cocktail_sort_list(listint_t **list)
 {
 	listint_t *prev, *current, *first_node, *last_node;
 
-	if (list == NULL)
+	if (list == NULL || (*list)->next == NULL)
 		return;
 
 	first_node = (*list);
