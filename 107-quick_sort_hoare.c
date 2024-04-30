6150f38f0430e741f1540258c48b36f6fd5a240b
flowsar <brahim.sar151@gmail.com>
2024-04-30 19:32:48 +0100

    quick sort Hoare

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -86,16 +86,3 @@ void Swap(int *array, size_t size, int i, int j)
 		print_array(array, size);
 	}
 }
-
-int main(void)
-{
-    int array[] = {87, 65, 28, 63, 93, 52, 39, 59, 27, 30, 24, 83, 69, 62, 13, 6, 88, 58, 92, 26, 42, 11, 16, 21, 75, 36, 71, 8, 45, 38};
-    size_t n = sizeof(array) / sizeof(array[0]);
-
-    print_array(array, n);
-    printf("\n");
-    quick_sort_hoare(array, n);
-    printf("\n");
-    print_array(array, n);
-    return (0);
-}
